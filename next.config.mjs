9812deafb20af0349d677b23a31ede9cff80a8b2
AhmedElhlawany <ahmedelhalawany429@gmail.com>
2025-10-08 12:20:49 +0300

    fix images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // لو بتستخدم GitHub Pages
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
+};
+
+module.exports = nextConfig;
